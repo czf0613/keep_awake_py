d970e7a47554fc6769fdc2914b1d61cda69998e6
Kevin Chen <1354016594@qq.com>
2025-11-13 22:01:32 +0800

    fix: add descriptions

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-
-rm -rf build || true
-rm -rf dist || true
-rm -rf src/*.egg-info || true
-
-uv build
--- src/keep_awake/_native_api.pyi
+++ src/keep_awake/_native_api.pyi
@@ -3,10 +3,10 @@
 def prevent_sleep() -> bool:
     """Prevent the system from sleeping. Returns True if successful, False otherwise.
     Now the screen will not turn off and system will not go to sleep.
-    This method is concurrent-safe.
+    This method is concurrent-safe on macOS, not safe on Windows(for some reasons).
     """
     pass
 
-def allow_sleep():
-    """Reset the power management state. This method is concurrent-safe."""
+def allow_sleep() -> None:
+    """Reset the power management state. This method is concurrent-safe on macOS, not safe on Windows."""
     pass
