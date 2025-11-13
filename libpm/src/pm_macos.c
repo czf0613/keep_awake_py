c03a244a029ad643b9bf4513d1df02ddc1f7a92e
Kevin Chen <1354016594@qq.com>
2025-11-13 21:06:48 +0800

    fix: fix windows compilation warnings

--- libpm/src/pm_macos.c
+++ libpm/src/pm_macos.c
@@ -1,6 +1,4 @@
 #include "pm.h"
-
-#ifdef __APPLE__
 #include <pthread.h>
 #include <IOKit/pwr_mgt/IOPMLib.h>
 #include <CoreFoundation/CoreFoundation.h>
@@ -37,35 +35,3 @@ PM_API void allow_sleep(void)
 
     pthread_mutex_unlock(&lock);
 }
-#endif // __APPLE__
-
-#ifdef _Win32
-#include <windows.h>
-
-static volatile bool flag = false;
-
-// Windows下的防止休眠本质上是靠新开启一个线程来实现的
-// 这个线程一直轮询防止休眠
-static void run_forever(void)
-{
-    while (flag)
-    {
-        SetThreadExecutionState(ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED);
-        Sleep(1000);
-    }
-}
-
-// 由于windows下的机制问题，Windows的防休眠与允许休眠都很难做到线程安全
-// 高频调用的情况下，的确可能会出现内存泄漏
-PM_API bool prevent_sleep(void)
-{
-    flag = true;
-    HANDLE handle = CreateThread(NULL, 0, run_forever, NULL, 0, NULL);
-    return handle != NULL;
-}
-
-PM_API void allow_sleep(void)
-{
-    flag = false;
-}
-#endif // _Win32
